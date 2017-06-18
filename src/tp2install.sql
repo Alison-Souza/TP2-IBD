a2a2eaefa39ff43722c7431ac1e991b5a871d795
Yuri Niitsuma <ignitzhjfk@gmail.com>
2017-06-18 02:42:21 -0300

    step

--- src/tp2install.sql
+++ src/tp2install.sql
@@ -1,57 +1,69 @@
-CREATE TABLE Partido(
-	id INT,
-	uf CHAR(2),
-	sigla CHAR(10),
-	PRIMARY KEY(id)
+drop database if exists ibd_tp;
+create database ibd_tp;
+use ibd_tp;
+
+drop table if exists Partido;
+create table Partido(
+  uf CHAR(2),
+  sigla VARCHAR(10),
+  PRIMARY KEY(uf,sigla)
 );
 
-CREATE TABLE Candidato(
-	cpf REAL,
-	idPartido INT,
-	numero INT,
-	cargo TEXT(100),
-	nome TEXT(100),
-	PRIMARY KEY(cpf),
-	FOREIGN KEY(idPartido) REFERENCES Partido(id)
+drop table if exists Candidato;
+create table Candidato(
+  cpf CHAR(11),
+  ufPartido CHAR(2),
+  siglaPartido VARCHAR(10),
+  numero INT,
+  cargo TEXT(100),
+  nome TEXT(100),
+  PRIMARY KEY(cpf),
+  FOREIGN KEY(ufPartido,siglaPartido) REFERENCES Partido(uf,sigla)
 );
 
-CREATE TABLE Doador(
-	cpf REAL,
-	nome TEXT(100),
-	PRIMARY KEY(cpf)
+drop table if exists Doador;
+create table Doador(
+  id VARCHAR(14),
+  nome TEXT(100),
+  PRIMARY KEY(id)
 );
 
-CREATE TABLE Partido_Comite(
-	idPartido INT,
-	nome TEXT(100),
-	PRIMARY KEY(idPartido, nome(100)),
-	FOREIGN KEY(idPartido) REFERENCES Partido(id)
+drop table if exists Partido_Comite;
+create table Partido_Comite(
+  ufPartido CHAR(2),
+  siglaPartido CHAR(10),
+  nome TEXT(100),
+  PRIMARY KEY(ufPartido,siglaPartido,nome(100)),
+  FOREIGN KEY(ufPartido,siglaPartido) REFERENCES Partido(uf,sigla)
 );
 
-CREATE TABLE Doacao_Partido(
-	doacaoID INT,
-	cpfDoador REAL,
-	idPartido INT,
-	tipoDoDocumento TEXT(100),
-	numeroDoDocumento TEXT(100),
-	data DATE,
-	valor REAL,
-	tipo TEXT(100),
-	PRIMARY KEY(doacaoID),
-	FOREIGN KEY(idPartido) REFERENCES Partido(id),
-	FOREIGN KEY(cpfDoador) REFERENCES Doador(cpf)
+drop table if exists Doacao_Partido;
+create table Doacao_Partido(
+  doacaoID INT,
+  cpfDoador VARCHAR(14),
+  ufPartido CHAR(2),
+  siglaPartido CHAR(10),
+  tipoDoDocumento TEXT(100),
+  numeroDoDocumento TEXT(100),
+  data DATE,
+  valor REAL,
+  tipo TEXT(100),
+  PRIMARY KEY(doacaoID),
+  FOREIGN KEY(ufPartido,siglaPartido) REFERENCES Partido(uf,sigla),
+  FOREIGN KEY(cpfDoador) REFERENCES Doador(id)
 );
-	
-CREATE TABLE Doacao_Candidato(
-	doacaoID INT,
-	cpfDoador REAL,
-	cpfCandidato REAL,
-	numeroReciboEleitoral TEXT(100),
-	numeroDocumento TEXT(100),
-	data DATE,
-	valor REAL,
-	tipo TEXT(100),
-	PRIMARY KEY(doacaoID),
-	FOREIGN KEY(cpfDoador) REFERENCES Doador(cpf),
-	FOREIGN KEY(cpfCandidato) REFERENCES Candidato(cpf)
+
+drop table if exists Doacao_Candidato;
+create table Doacao_Candidato(
+  doacaoID INT,
+  cpfDoador VARCHAR(14),
+  cpfCandidato CHAR(11),
+  numeroReciboEleitoral TEXT(100),
+  numeroDocumento TEXT(100),
+  data DATE,
+  valor REAL,
+  tipo TEXT(100),
+  PRIMARY KEY(doacaoID),
+  FOREIGN KEY(cpfDoador) REFERENCES Doador(id),
+  FOREIGN KEY(cpfCandidato) REFERENCES Candidato(cpf)
 );
